8a1ff7a840a7da0a36da1a2445e76e87ed515c8a
Lithrein <lithrein.site@gmail.com>
2023-09-17 02:07:01 +0900

    add comparison operators support

--- clisp.lisp
+++ clisp.lisp
@@ -37,6 +37,25 @@ NAME and SUBSTITUTION are both strings and should enclosed in double-quotes.
 NAME can contain parenthesis just like a C macro does, i.e., no spaces before
 the parenthesis.  SUBSTITUTION should be valid C code."
   (format to-stream "#define ~{~a~^ ~}~%" form))
+
+(defun compile-cmp-op (form to-stream &optional (cpp nil))
+  "Compile a FORM beginning with a compare operator (< > <= >= > ==) into a valid
+C expression and write it on TO-STREAM.
+The CPP parameter enable C Preprocessor mode, restricting what can appear in
+the expression."
+  (let ((compile-proxy (if cpp #'compile-cpp-cond-expr #'compile-form)))
+    (destructuring-bind (op &rest args) form
+      (format to-stream "(")
+      (do ((cur args (cdr cur)))
+          ((not (cdr cur)))
+        (format to-stream "(")
+        (funcall compile-proxy (car cur) to-stream)
+        (format to-stream " ~a " op)
+        (funcall compile-proxy (cadr cur) to-stream)
+        (format to-stream ")")
+        (when (cddr cur)
+          (format to-stream " && ")))
+      (format to-stream ")"))))
 (defun compile-form (form to-stream)
   "Compile a clisp FROM and write it on TO-STREAM"
   (if (consp form)
@@ -44,6 +63,8 @@ the parenthesis.  SUBSTITUTION should be valid C code."
         (cond
           ((string= "%include" (string op)) (compile-include args to-stream))
           ((string= "%define" (string op)) (compile-define args to-stream))
+          ((member (string op) '("<" ">" "<=" ">=" ">" "=" "&&" "||") :test #'equal)
+           (compile-cmp-op form to-stream))
           (t (format t "unknown construct ~a" (car form)))))
       (format to-stream "~a" form)))
 
