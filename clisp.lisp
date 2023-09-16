a7769dba68815117f539cbc4d243ff1f8aceb0e5
Lithrein <lithrein.site@gmail.com>
2023-09-17 02:01:40 +0900

    improve reader and rewrite the main loop

--- clisp.lisp
+++ clisp.lisp
@@ -1,8 +1,21 @@
 (in-package #:clisp)
 
-(defmacro with-case-sensitivity (&body body)
-  "Enable case sensitivity while performing a READ."
+(defmacro with-custom-reader (ignore-chars &body body)
+  "Enable case sensitivity and disable the reader macros associated to
+characters in the IGNORE-CHARS string while performing READ."
   `(let ((*readtable* (copy-readtable)))
+     (loop for cc across ,ignore-chars do
+       (set-macro-character
+        cc
+        (lambda (stream char)
+          (concatenate
+           'string
+           (make-string 1 :initial-element char)
+           (loop for c = (peek-char nil stream nil nil)
+                 while (and c (eql c (peek-char t stream nil nil)))
+                 collect (read-char stream) into letters
+                 finally (return (coerce letters 'string)))))
+        t *readtable*))
      (setf (readtable-case *readtable*) :preserve)
      (progn
        ,@body)))
@@ -17,15 +30,21 @@ or in angle-brackets."
 		(cond ((symbolp filename) "#include ~a~%")
 		      (t "#include \"~a\"~%"))
 		filename)))
+(defun compile-form (form to-stream)
+  "Compile a clisp FROM and write it on TO-STREAM"
+  (if (consp form)
+      (destructuring-bind (op &rest args) form
+        (cond
+          ((string= "%include" (string op)) (compile-include args to-stream))
+          (t (format t "unknown construct ~a" (car form)))))
+      (format to-stream "~a" form)))
 
 (defun compile-clisp (from-stream to-stream)
   "Write the result of the compilation of the content of FROM-STREAM into
 TO-STREAM."
-  (loop for form = (with-case-sensitivity (read from-stream nil))
-        while form do
-        (switch ((string (car form)) :test #'equal)
-                ("%include" (compile-include (cdr form) to-stream))
-                (otherwise (format t "unknown construct ~a" (car form))))))
+  (loop for form = (with-custom-reader "'|" (read from-stream nil))
+        while form do (compile-form form to-stream)))
 
 (defun main ()
+  "The entry point."
   (compile-clisp *standard-input* *standard-output*))
