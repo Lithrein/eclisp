3b677f750b1e5524e9cffffeb1d3029342f70f4c
Lithrein <lithrein.site@gmail.com>
2024-01-24 05:17:26 +0900

    t: refactor the test driver

--- t/main.lisp
+++ t/main.lisp
@@ -38,87 +38,12 @@ At the end the RESULT file is deleted."
 (test
  conformance-tests
  "Assert the conformance of the implementation"
- (is (compare-to-ref (compile-to-file "t/if.eclisp"
-                                      "t/if.c.tmp")
-                     "t/if.c"))
- (is (compare-to-ref (compile-to-file "t/dot.eclisp"
-                                      "t/dot.c.tmp")
-                     "t/dot.c"))
- (is (compare-to-ref (compile-to-file "t/break.eclisp"
-                                      "t/break.c.tmp")
-                     "t/break.c"))
- (is (compare-to-ref (compile-to-file "t/continue.eclisp"
-                                      "t/continue.c.tmp")
-                     "t/continue.c"))
- (is (compare-to-ref (compile-to-file "t/cast.eclisp"
-                                      "t/cast.c.tmp")
-                     "t/cast.c"))
- (is (compare-to-ref (compile-to-file "t/addr.eclisp"
-                                      "t/addr.c.tmp")
-                     "t/addr.c"))
- (is (compare-to-ref (compile-to-file "t/switch.eclisp"
-                                      "t/switch.c.tmp")
-                     "t/switch.c"))
- (is (compare-to-ref (compile-to-file "t/deref.eclisp"
-                                      "t/deref.c.tmp")
-                     "t/deref.c"))
- (is (compare-to-ref (compile-to-file "t/for.eclisp"
-                                      "t/for.c.tmp")
-                     "t/for.c"))
- (is (compare-to-ref (compile-to-file "t/label.eclisp"
-                                      "t/label.c.tmp")
-                     "t/label.c"))
- (is (compare-to-ref (compile-to-file "t/goto.eclisp"
-                                      "t/goto.c.tmp")
-                     "t/goto.c"))
- (is (compare-to-ref (compile-to-file "t/seq.eclisp"
-                                      "t/seq.c.tmp")
-                     "t/seq.c"))
- (is (compare-to-ref (compile-to-file "t/defstruct.eclisp"
-                                      "t/defstruct.c.tmp")
-                     "t/defstruct.c"))
- (is (compare-to-ref (compile-to-file "t/enum.eclisp"
-                                      "t/enum.c.tmp")
-                     "t/enum.c"))
- (is (compare-to-ref (compile-to-file "t/while.eclisp"
-                                      "t/while.c.tmp")
-                     "t/while.c"))
- (is (compare-to-ref (compile-to-file "t/do-while.eclisp"
-                                      "t/do-while.c.tmp")
-                     "t/do-while.c"))
- (is (compare-to-ref (compile-to-file "t/return.eclisp"
-                                      "t/return.c.tmp")
-                     "t/return.c"))
- (is (compare-to-ref (compile-to-file "t/hello.eclisp"
-                                      "t/hello.c.tmp")
-                     "t/hello.c"))
- (is (compare-to-ref (compile-to-file "t/arrow.eclisp"
-                                      "t/arrow.c.tmp")
-                     "t/arrow.c"))
- (is (compare-to-ref (compile-to-file "t/aref.eclisp"
-                                      "t/aref.c.tmp")
-                     "t/aref.c"))
- (is (compare-to-ref (compile-to-file "t/function.eclisp"
-                                      "t/function.c.tmp")
-                     "t/function.c"))
- (is (compare-to-ref (compile-to-file "t/set.eclisp"
-                                      "t/set.c.tmp")
-                     "t/set.c"))
- (is (compare-to-ref (compile-to-file "t/types.eclisp"
-                                      "t/types.c.tmp")
-                     "t/types.c"))
- (is (compare-to-ref (compile-to-file "t/defvar.eclisp"
-                                      "t/defvar.c.tmp")
-                     "t/defvar.c"))
- (is (compare-to-ref (compile-to-file "t/include.eclisp"
-				      "t/include.c.tmp")
-		     "t/include.c"))
- (is (compare-to-ref (compile-to-file "t/includes.eclisp"
-				      "t/includes.c.tmp")
-		     "t/include.c"))
- (is (compare-to-ref (compile-to-file "t/cpp-define.eclisp"
-                                      "t/cpp-define.c.tmp")
-		     "t/cpp-define.c"))
- (is (compare-to-ref (compile-to-file "t/cpp-if.eclisp"
-                                      "t/cpp-if.c.tmp")
-                     "t/cpp-if.c")))
+(let ((test-names '("if" "dot" "break" "continue" "cast" "addr" "switch"
+                    "deref" "for" "label" "goto" "seq" "defstruct" "enum"
+                    "while" "do-while" "return" "hello" "arrow" "aref"
+                    "function" "set" "types" "defvar" "include" "includes"
+                    "cpp-define" "cpp-if")))
+ (loop for test-name in test-names do 
+       (is (compare-to-ref (compile-to-file (format nil "t/~a.eclisp" test-name)
+                                            (format nil "t/~a.c.tmp" test-name))
+                           (format nil "t/~a.c" test-name))))))
